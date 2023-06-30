d5a29c005db40d7074d0a78509b3d7425bf52b01
amesar <amesar@users.noreply.github.co>
2023-06-30 07:47:30 -0400

    Issue #6: standardized web UI link tag name

--- mlflow_reports/data/link_utils.py
+++ mlflow_reports/data/link_utils.py
@@ -2,28 +2,29 @@ from mlflow_reports.client.http_client import MlflowHttpClient
 
 http_client = MlflowHttpClient()
 
+_UI_LINK_TAG = "_web_ui_link"
 
 def add_run_link(run):
     info = run["info"]
     experiment_id = info["experiment_id"]
     run_id = info["run_id"]
     link = f"{_mk_mlflow_link()}/experiments/{experiment_id}/runs/{run_id}"
-    run["info"]["_run_ui_link"] = link
+    run["info"][_UI_LINK_TAG] = link
 
 
 def add_experiment_link(exp):
     link = f'{_mk_mlflow_link()}/experiments/{exp["experiment_id"]}'
-    exp["_experiment_ui_link"] = link
+    exp[_UI_LINK_TAG] = link
 
 
 def add_model_version_link(version):
     link = f'{_mk_mlflow_link()}/models/{version["name"]}/versions/{version["version"]}'
-    version["_model_version_ui_link"] = link
+    version[_UI_LINK_TAG] = link
 
 
 def add_registered_model_link(reg_model):
     link = f'{_mk_mlflow_link()}/models/{reg_model["name"]}'
-    reg_model["_registered_model_ui_link"] = link
+    reg_model[_UI_LINK_TAG] = link
 
 
 def _mk_mlflow_link():
