5d9c19aa9fcd642f9ab7831e8d2b625e1690ff18
amesar <amesar@users.noreply.github.co>
2023-07-05 23:23:40 -0400

    Issue #6: major refactor of registered-model and model-version links

--- mlflow_reports/data/link_utils.py
+++ mlflow_reports/data/link_utils.py
@@ -2,6 +2,7 @@ from mlflow.utils.databricks_utils import get_workspace_info_from_dbutils
 from mlflow_reports.client.http_client import get_mlflow_client
 from mlflow_reports.common.mlflow_utils import is_unity_catalog_model
 
+
 http_client = get_mlflow_client()
 
 _workspace_host, _workspace_id = get_workspace_info_from_dbutils()
@@ -9,75 +10,70 @@ _workspace_host, _workspace_id = get_workspace_info_from_dbutils()
 _UI_LINK_TAG = "_web_ui_link"
 _API_LINK_TAG= "_api_link"
 
+
+# == MLflow object linkers
+
 def add_run_links(run):
     info = run["info"]
     experiment_id = info["experiment_id"]
     run_id = info["run_id"]
-    link = f"{_mk_mlflow_link()}/experiments/{experiment_id}/runs/{run_id}"
+    link = f"{_get_mlflow_ui_base()}/experiments/{experiment_id}/runs/{run_id}"
     run["info"][_UI_LINK_TAG] = link
-    link = f"{http_client}/runs/get?run_id={run_id}"
+    link = f"{http_client.get_api_uri()}/runs/get?run_id={run_id}"
     run["info"][_API_LINK_TAG] = link
 
 
 def add_experiment_links(exp):
     experiment_id = exp["experiment_id"]
-    link = f"{_mk_mlflow_link()}/experiments/{experiment_id}"
+    link = f"{_get_mlflow_ui_base()}/experiments/{experiment_id}"
     exp[_UI_LINK_TAG] = link
-    link = f"{http_client}/experiments/get?experiment_id={experiment_id}"
+    link = f"{http_client.get_api_uri()}/experiments/get?experiment_id={experiment_id}"
     exp[_API_LINK_TAG] = link
 
 
 def add_model_version_links(version):
-    model_name = version["name"]
-    vr = version["version"]
-    if is_unity_catalog_model(model_name):
-        ui_link = f"{_mk_uc_model_ui_uri(model_name)}/version/{vr}"
-        uc_component = "unity-catalog"
-    else:
-        ui_link = f"{_mk_mlflow_link()}/models/{model_name}/versions/{vr}"
-        uc_component = ""
-    version[_UI_LINK_TAG] = ui_link
-    api_link = f"{_get_api_uri()}/{uc_component}model-versions/get?name={model_name}&version={vr}"
-    version[_API_LINK_TAG] = api_link
+    _mk_uc_links(version, version["name"], version["version"])
 
 
 def add_registered_model_links(reg_model):
-    model_name = reg_model["name"]
+    _mk_uc_links(reg_model, reg_model["name"])
+
+
+# == Helpers
+
+def _mk_uc_links(dct, model_name, version=None):
+    from requests.utils import quote
+    from urllib.parse import urlencode
+
+    _model_name = quote(model_name)
     if is_unity_catalog_model(model_name):
-        ui_link = _mk_uc_model_ui_uri(model_name)
-        uc_component = "unity-catalog"
+        # TODO: need to add "?o={workspace_id}"
+        ui_link = f"{_get_host_name()}/explore/data/models/{_model_name}"
+        uc_component = "unity-catalog/"
+        vresource = "version"
     else:
-        ui_link = f"{_mk_mlflow_link()}/models/{model_name}"
+        ui_link = f"{_get_mlflow_ui_base()}/models/{_model_name}"
         uc_component = ""
-    reg_model[_UI_LINK_TAG] = ui_link
-    api_link = f"{_get_api_uri()}/{uc_component}/registered-models/get?name={model_name}"
-    reg_model[_API_LINK_TAG] = api_link
+        vresource = "versions"
 
-
-def _mk_uc_model_ui_uri(model_name):
-    # TODO: need to add "?o={workspace_id}"
-    # https://e2-demo-west.cloud.databricks.com/explore/data/models/my_catalog.default.my_model
-    return f"{_get_host_name()}/explore/data/models/{model_name}"
-
-def _get_api_uri():
-    """
-    MLflow API base: https://e2-demo-west.cloud.databricks.com/api/2.0/mlflow
-    """
-    from mlflow_reports.client.http_client import UnityCatalogHttpClient
-    if isinstance(http_client, UnityCatalogHttpClient):
-        return str(http_client._mlflow_client)
+    params = { "name": model_name }
+    if version:
+        resource = "model-versions"
+        params["version"] = version
+        ui_link += f"/{vresource}/{version}"
     else:
-        return str(http_client)
+        resource = "registered-models"
+    qp = urlencode(params)
+    api_link = f"{http_client.get_api_uri()}/{uc_component}{resource}/get?{qp}"
 
-def _get_host_name():
-    """
-    Workspace host name: https://e2-demo-west.cloud.databricks.com
-    """
-    client_uri = _get_api_uri()
-    idx = client_uri.find("/api/")
-    return client_uri[0:idx]
+    dct[_UI_LINK_TAG] = ui_link
+    dct[_API_LINK_TAG] = api_link
 
-def _mk_mlflow_link():
+def _get_mlflow_ui_base():
+    """
+    OSS:        http://localhost:5020#
+    Databricks: https://e2-demo-west.cloud.databricks.com#mlflow 
+    """
     mlflow_uri = _get_host_name()
     if _workspace_host: # inside Databricks, e.g. "https://c3-south.mist.databricks.com"
         mlflow_uri = f"{_workspace_host}#mlflow"
@@ -87,3 +83,12 @@ def _mk_mlflow_link():
         else: # calling Databricks externally
             mlflow_uri += "#mlflow"
     return mlflow_uri
+
+def _get_host_name():
+    """
+    OSS:        http://localhost:5020
+    Databricks: https://e2-demo-west.cloud.databricks.com
+    """
+    client_uri = http_client.get_api_uri()
+    idx = client_uri.find("/api/")
+    return client_uri[0:idx]
