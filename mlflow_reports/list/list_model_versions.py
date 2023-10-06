6c2a34be4ea7735c395d9c15f4552ecee145a11a
amesar <amesar@users.noreply.github.co>
2023-10-06 06:02:42 -0400

    list_model_versions.py: sort results by model name and version since UC doesn't return sorted list

--- mlflow_reports/list/list_model_versions.py
+++ mlflow_reports/list/list_model_versions.py
@@ -40,6 +40,7 @@ def show(filter,
     if "description" in df and max_description:
         df["description"] = df["description"].str[:max_description]
 
+    df = df.sort_values(["name", "version"], ascending=[True, False])
     list_utils.show_and_write(df, columns, output_csv_file)
 
 
