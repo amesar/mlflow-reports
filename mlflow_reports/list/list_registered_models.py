999b8050e40d9fcd02d5e25e1ecd893b8ae1e543
amesar <amesar@users.noreply.github.co>
2023-10-08 11:59:59 -0400

    list_registered_models.py: fixed multiple columns parsing bug

--- mlflow_reports/list/list_registered_models.py
+++ mlflow_reports/list/list_registered_models.py
@@ -30,6 +30,8 @@ def show(filter,
         max_description,
         output_csv_file
     ):
+    if isinstance(columns, str):
+        columns = columns.split(",")
     df = search_registered_models.search(filter, prefix, get_tags_and_aliases, tags_and_aliases_as_string, unity_catalog)
     if "description" in df and max_description:
         df["description"] = df["description"].str[:max_description]
@@ -48,27 +50,21 @@ def show(filter,
 @opt_output_csv_file
 
 def main(
-        filter, 
-        prefix, 
-        get_tags_and_aliases, 
-        tags_and_aliases_as_string, 
-        unity_catalog, 
-        columns, 
-        max_description, 
+        filter,
+        prefix,
+        get_tags_and_aliases,
+        tags_and_aliases_as_string,
+        unity_catalog,
+        columns,
+        max_description,
         output_csv_file
     ):
     print("Options:")
     args = locals()
     for k,v in args.items():
         print(f"  {k}: {v}")
-    #if columns:
-        #args["columns"] = columns.split(",")
     show(**args)
 
-    #print("Options:")
-    #for k,v in locals().items():
-        #print(f"  {k}: {v}")
-    #show(filter, prefix, get_tags_and_aliases, tags_and_aliases_as_string, unity_catalog, max_description, output_csv_file)
 
 if __name__ == "__main__":
     main()
