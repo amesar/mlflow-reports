818ee687b398438735e4ed25d8eb49c3a2a207cf
amesar <amesar@users.noreply.github.co>
2023-10-09 23:13:42 -0400

    Issue #18: Updated Create_MLflow_Database.py to include both models and versions tables

--- databricks_notebooks/sql_analytics/Create_MLflow_Database.py
+++ databricks_notebooks/sql_analytics/Create_MLflow_Database.py
@@ -1,17 +1,16 @@
 # Databricks notebook source
-# MAGIC %md ## Create a table from all registered models
+# MAGIC %md ## Create MLflow Database
 # MAGIC
-# MAGIC **Overview**
-# MAGIC * Create a table from all registered models.
+# MAGIC ##### Overview
 # MAGIC
-# MAGIC **Widgets**
-# MAGIC * `Table` - Full table name.
-# MAGIC * `Table drop` - Drop table before write (because of potential schema conflict on write).
-# MAGIC * `Unity Catalog` - Use Unity Catalog.
+# MAGIC Create a database for registered models and model versions as returned by the MLflow API.
+# MAGIC
+# MAGIC ##### Widgets
+# MAGIC * `Database` - full path name such as `my_catalog.mlflow_uc`.
 
 # COMMAND ----------
 
-# MAGIC %md #### Setup
+# MAGIC %md ### Setup
 
 # COMMAND ----------
 
@@ -19,92 +18,167 @@
 
 # COMMAND ----------
 
-#dbutils.widgets.removeAll()
+dbutils.widgets.text("Database", "")
+database_name = dbutils.widgets.get("Database")
+print("database_name:", database_name)
 
 # COMMAND ----------
 
-dbutils.widgets.text("Table", "")
-dbutils.widgets.dropdown("Table drop", "no", ["yes","no"])
-dbutils.widgets.dropdown("Unity Catalog", "no", ["yes", "no"])
+assert_widget(database_name, "Database")
 
-table_name = dbutils.widgets.get("Table")
-drop_table = dbutils.widgets.get("Table drop") == "yes"
+is_unity_catalog = len(database_name.split(".")) == 2
+models_table = f"{database_name}.models"
+versions_table = f"{database_name}.versions"
 
-unity_catalog = dbutils.widgets.get("Unity Catalog") == "yes"
-
-print("table_name:", table_name)
-print("drop_table:", drop_table)
-print("unity_catalog:", unity_catalog)
+print("is_unity_catalog:", is_unity_catalog)
+print("models_table:", models_table)
+print("versions_table:", versions_table)
 
 # COMMAND ----------
 
-assert_widget(table_name, "Table")
+# MAGIC %md ### Registered models table
 
 # COMMAND ----------
 
-# MAGIC %md #### Get all registered models 
+# MAGIC %md ##### Call API
 
 # COMMAND ----------
 
 from mlflow_reports.list import search_registered_models
 
-pandas_df = search_registered_models.search(
-    unity_catalog = unity_catalog,
+pdf = search_registered_models.search(
+    unity_catalog = is_unity_catalog,
     tags_and_aliases_as_string = True
 )
+df = spark.createDataFrame(pdf)
 
 # COMMAND ----------
 
-pandas_df
+# MAGIC %md ##### Tweak columns
 
 # COMMAND ----------
 
-from pyspark.sql.types import MapType, StringType
-from pyspark.sql.functions import from_json
+from pyspark.sql.functions import *
 
-df = spark.createDataFrame(pandas_df)
+df = df\
+  .withColumn("creation_timestamp",date_format("creation_timestamp", "yyyy-MM-dd hh:mm:ss")) \
+  .withColumn("last_updated_timestamp",date_format("creation_timestamp", "yyyy-MM-dd hh:mm:ss"))
+
 if "tags" in df.columns:
+    from pyspark.sql.types import MapType, StringType
+    from pyspark.sql.functions import from_json
     df = df.withColumn("tags", from_json(df.tags, MapType(StringType(), StringType())))
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show registered models tables
+
+# COMMAND ----------
 
 display(df)
 
 # COMMAND ----------
 
-# MAGIC %md #### Create table
+# MAGIC %md ##### Write to table
 
 # COMMAND ----------
 
-if drop_table:
-    spark.sql(f"drop table if exists {table_name}")
+spark.sql(f"drop table if exists {models_table}")
+df.write.mode("overwrite").saveAsTable(f"{models_table}")
 
 # COMMAND ----------
 
-df.write.mode("overwrite").saveAsTable(table_name)
+# MAGIC %md ##### Describe table
 
 # COMMAND ----------
 
-# MAGIC %md #### Describe table
+df = spark.sql(f"describe extended {models_table}")
+display(df)
 
 # COMMAND ----------
 
-# MAGIC %sql describe extended ${Table} 
+# MAGIC %md ### Model Versions table
 
 # COMMAND ----------
 
-# MAGIC %sql describe history ${Table} 
+# MAGIC %md ##### Call API
 
 # COMMAND ----------
 
-# MAGIC %md #### Query table
+from mlflow_reports.list import search_model_versions
+
+pdf = search_model_versions.search(
+    unity_catalog = is_unity_catalog,
+    tags_and_aliases_as_string = True,
+    get_model_details = False
+)
+df_versions = spark.createDataFrame(pdf)
 
 # COMMAND ----------
 
-# MAGIC %sql select * from ${Table} order by name
+# MAGIC %md ##### Tweak columns
 
 # COMMAND ----------
 
-# MAGIC %sql select user_id, * from ${Table} order by user_id
+df = df_versions
+
+df = df\
+  .withColumn("creation_timestamp",date_format("creation_timestamp", "yyyy-MM-dd hh:mm:ss")) \
+  .withColumn("creation_timestamp",date_format("creation_timestamp", "yyyy-MM-dd hh:mm:ss"))
+
+if "tags" in df.columns:
+    df = df.withColumn("tags", from_json(df.tags, MapType(StringType(), StringType())))
 
 # COMMAND ----------
 
-# MAGIC %sql select user_id, count(*) as num_models from ${Table} group by user_id order by num_models desc
+# MAGIC %md ##### Show model versions table
+# MAGIC
+# MAGIC display(df)
+
+# COMMAND ----------
+
+spark.sql(f"drop table if exists {versions_table}")
+df.write.mode("overwrite").saveAsTable(f"{versions_table}")
+
+# COMMAND ----------
+
+# MAGIC %md ##### Describe model versions table
+
+# COMMAND ----------
+
+df = spark.sql(f"describe extended {versions_table}")
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md ### Some SQL queries 
+
+# COMMAND ----------
+
+spark.sql(f"use {database_name}")
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show table count
+
+# COMMAND ----------
+
+# MAGIC %sql select count(*) as num_models from models;
+
+# COMMAND ----------
+
+# MAGIC %sql select count(*) as num_versions from versions;
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show number of models per user
+
+# COMMAND ----------
+
+# MAGIC %sql select user_id, count(*) as num_models from models group by user_id order by num_models desc
+
+# COMMAND ----------
+
+# MAGIC %md ### See next notebook
+# MAGIC
+# MAGIC TO query tables, see the [SQL_Queries]($SQL_Queries) notebook.
