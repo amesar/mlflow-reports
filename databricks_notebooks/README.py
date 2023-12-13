e9135dcc0d23b5f131a151c343180f259d4c3e70
amesar <amesar>
2023-12-13 17:15:35 +0000

    Added List_Model_Serving_Endpoints notebook

--- databricks_notebooks/README.py
+++ databricks_notebooks/README.py
@@ -17,8 +17,9 @@
 # MAGIC ##### MLflow list objects notebooks
 # MAGIC * [List_Registered_Models]($objects/List_Registered_Models)
 # MAGIC * [List_Model_Versions]($objects/List_Model_Versions)
-# MAGIC * [List_Vector_Search_Endpoints]($objects/List_Vector_Search_Endpoints) - SQL queries for Vector Search endpoints
-# MAGIC * [List_Gateway_Routes]($objects/List_Gateway_Routes) - SQL queries for AI Gateway routes
+# MAGIC * [List_Model_Serving_Endpoints]($List_Model_Serving_Endpoints) - List and query model serving endpoints
+# MAGIC * [List_Vector_Search_Endpoints]($objects/List_Vector_Search_Endpoints) - List and query Vector Search endpoints
+# MAGIC * [List_Gateway_Routes]($objects/List_Gateway_Routes) - List and query AI Gateway routes
 # MAGIC
 # MAGIC ##### MLflow MLmodel notebooks
 # MAGIC * [Download_MLmodel]($objects/Download_MLmodel) - Download and display a model version's MLmodel artifact.
@@ -38,4 +39,4 @@
 # MAGIC * https://github.com/amesar/mlflow-reports
 # MAGIC * https://github.com/amesar/mlflow-reports/tree/master/databricks_notebooks
 # MAGIC
-# MAGIC Last updated: 2023-12-05
+# MAGIC Last updated: 2023-12-13
---
+++ databricks_notebooks/objects/List_Model_Serving_Endpoints.py
@@ -0,0 +1,45 @@
+# Databricks notebook source
+# MAGIC %md ## List Model Serving Endpoints
+# MAGIC * https://docs.databricks.com/api/workspace/servingendpoints
+# MAGIC * [api/2.0/serving-endpoints](https://docs.databricks.com/api/workspace/servingendpoints/list)
+
+# COMMAND ----------
+
+# MAGIC %run ../Common
+
+# COMMAND ----------
+
+# MAGIC %md #### Create endpoints dataframe
+
+# COMMAND ----------
+
+from mlflow_reports.client.model_serving_client import ModelServingClient
+
+# COMMAND ----------
+
+client = ModelServingClient()
+
+rsp = client.list_endpoints()
+endpoints = rsp["endpoints"]
+len(endpoints)
+
+# COMMAND ----------
+
+df = to_dataframe(endpoints)
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md #### SQL queries
+
+# COMMAND ----------
+
+df.createOrReplaceTempView("endpoints") 
+
+# COMMAND ----------
+
+# MAGIC %md ##### Summary
+
+# COMMAND ----------
+
+# MAGIC %sql select name, creator, creation_timestamp from endpoints order by creation_timestamp desc
