3f36f48cfd2ba10cfd626654b3db643518c9e354
amesar <amesar@users.noreply.github.co>
2024-02-04 14:49:34 -0500

    endpoints.list_endpoints: Added support for MlflowDeploymentClient returns obejcts instead of a dict like DatabricksDeploymentClient does

--- mlflow_reports/endpoints/list_endpoints.py
+++ mlflow_reports/endpoints/list_endpoints.py
@@ -5,21 +5,41 @@ Create and show a Pandas dataframe and text table fromt the JSON response for al
 import click
 import pandas as pd
 
+from mlflow_reports.common import MlflowReportsException
 from mlflow_reports.list.click_options import opt_columns, opt_output_csv_file
 from mlflow_reports.list import list_utils
 from . click_options import opt_call_databricks_model_serving
 from . import get_endpoint_client
 
+from mlflow.deployments.mlflow import MlflowDeploymentClient
+from mlflow.deployments.databricks import DatabricksDeploymentClient
+
 
 def create_pandas_df(call_databricks_model_serving=False):
     client = get_endpoint_client(call_databricks_model_serving)
     endpoints = client.list_endpoints()
-    if isinstance(endpoints, dict): # NOTE: Databricks api/2.0/serving-endpoints returns dict and not list
-        endpoints = endpoints["endpoints"]
-    df = pd.json_normalize(endpoints)
-    list_utils.to_datetime(df, "creation_timestamp")
-    list_utils.to_datetime(df, "last_updated_timestamp")
+    if isinstance(client, MlflowDeploymentClient):
+        endpoints = _to_dict(endpoints)
+        df = pd.json_normalize(endpoints)
+    elif isinstance(client, DatabricksDeploymentClient):
+        if isinstance(endpoints, dict): # NOTE: Databricks api/2.0/serving-endpoints returns dict and not list
+            endpoints = endpoints["endpoints"]
+        df = pd.json_normalize(endpoints)
+        list_utils.to_datetime(df, "creation_timestamp")
+        list_utils.to_datetime(df, "last_updated_timestamp")
+    else:
+        raise MlflowReportsException(message=f"Unsupported MLflow Deployments client: {type(client)}")
     return df
+
+
+def _to_dict(endpoints):
+    def convert(ep):
+        _ep = { k:v for k,v in ep.model.__dict__.items() if k != "model" }
+        model = { k:v for k,v in ep.model.__dict__.items() }
+        _ep["model"] = model
+        return _ep
+    return [ convert(ep) for ep in endpoints ]
+
 
 @click.command()
 @opt_columns
@@ -30,7 +50,7 @@ def main(columns, output_csv_file, call_databricks_model_serving):
     for k,v in locals().items():
         print(f"  {k}: {v}")
     df = create_pandas_df(call_databricks_model_serving)
-    print(f"Found {df.shape[0]} routes")
+    print(f"Found {df.shape[0]} endpoints")
     if isinstance(columns, str):
         columns = columns.split(",")
     list_utils.show_and_write(df, columns, output_csv_file)
