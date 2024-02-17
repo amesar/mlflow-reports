3635a63189ee722d530053f45a4ff75d41903328
amesar <amesar@users.noreply.github.co>
2024-02-17 17:44:04 -0500

    endpoints/list_endpoints.py: added get-details option to fetch details for each endpoint object

--- mlflow_reports/endpoints/list_endpoints.py
+++ mlflow_reports/endpoints/list_endpoints.py
@@ -4,10 +4,10 @@ Create and show a Pandas dataframe and text table from the JSON response for all
 
 import click
 from mlflow_reports.common import io_utils
-from mlflow_reports.common.click_options import opt_output_file_base, opt_get_raw
+from mlflow_reports.common.click_options import opt_output_file_base, opt_get_raw, opt_get_details
 from mlflow_reports.list.click_options import opt_columns, opt_normalized_pandas_df
 from . click_options import opt_call_databricks_model_serving
-from . import _list_endpoints
+from . import _list_endpoints, get_endpoint_client
 
 
 def list_endpoints(
@@ -15,9 +15,13 @@ def list_endpoints(
         output_file_base,
         call_databricks_model_serving = False,
         get_raw = False,
+        get_details = False,
         normalized_pandas_df = False
     ):
     endpoints = _list_endpoints(call_databricks_model_serving)
+    if get_details:
+        client = get_endpoint_client(call_databricks_model_serving)
+        endpoints = [ client.get_endpoint(ep["name"]) for ep in endpoints ]
     ts_columns = [] if get_raw else [ "creation_timestamp", "last_updated_timestamp" ]
     io_utils.write_csv_and_json_files(output_file_base, endpoints, columns, ts_columns, normalized_pandas_df)
 
@@ -27,14 +31,22 @@ def list_endpoints(
 @opt_output_file_base
 @opt_call_databricks_model_serving
 @opt_get_raw
+@opt_get_details
 @opt_normalized_pandas_df
-def main(columns, output_file_base, call_databricks_model_serving, get_raw=False, normalized_pandas_df=False):
+def main(
+        columns, 
+        output_file_base, 
+        call_databricks_model_serving, 
+        get_raw = False, 
+        get_details = False, 
+        normalized_pandas_df = False
+    ):
     print("Options:")
     for k,v in locals().items():
         print(f"  {k}: {v}")
     if isinstance(columns, str):
         columns = columns.split(",")
-    list_endpoints(columns, output_file_base, call_databricks_model_serving, get_raw, normalized_pandas_df)
+    list_endpoints(columns, output_file_base, call_databricks_model_serving, get_raw, get_details, normalized_pandas_df)
 
 
 if __name__ == "__main__":
