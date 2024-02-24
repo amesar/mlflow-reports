8c501420ed9b3e005b97b9aae9cbd2f7a1c6feae
amesar <amesar>
2024-02-24 23:08:01 +0000

    Added new List_Vector_Search_Indexes notebook

--- databricks_notebooks/README.py
+++ databricks_notebooks/README.py
@@ -1,6 +1,17 @@
 # Databricks notebook source
 # MAGIC %md ## README - mlflow-reports - Databricks notebooks
 # MAGIC
+# MAGIC
+# MAGIC ##### Git Repo
+# MAGIC * https://github.com/amesar/mlflow-reports
+# MAGIC * https://github.com/amesar/mlflow-reports/tree/master/databricks_notebooks
+# MAGIC
+# MAGIC #####  Last updated: _2024-02-24_
+
+# COMMAND ----------
+
+# MAGIC %md ### MLflow
+# MAGIC
 # MAGIC #####  Get MLflow object notebooks
 # MAGIC * [Get_Model_Version]($objects/Get_Model_Version) - [samples](https://github.com/amesar/mlflow-reports/tree/master/samples/databricks/mlflow_objects/model_versions)
 # MAGIC * [Get_Registered_Model]($objects/Get_Registered_Model) - [samples](https://github.com/amesar/mlflow-reports/tree/master/samples/databricks/mlflow_objects/registered_models)
@@ -12,20 +23,6 @@
 # MAGIC * [List_Model_Versions]($list/List_Model_Versions) - List and query model versions.
 # MAGIC * [List_Experiments]($list/List_Experiments) - List and query experiments.
 # MAGIC
-# MAGIC ##### Endpoint notebooks
-# MAGIC * Model Serving endpoints aka MLflow deployment endpoints
-# MAGIC   * [List_Model_Serving_Endpoints]($endpoints/List_Model_Serving_Endpoints) - List and query endpoints.
-# MAGIC     * [List_Model_Serving_Endpoint_Entities]($endpoints/List_Model_Serving_Endpoint_Entities) - List and query endpoint entities.
-# MAGIC     * [List_Model_Serving_Endpoint_Models]($endpoints/List_Model_Serving_Endpoint_Models) - List and query endpoint models/entities broken down by entity type.
-# MAGIC   * [Get_Model_Serving_Endpoint]($endpoints/Get_Model_Serving_Endpoint) 
-# MAGIC * Vector Search endpoints
-# MAGIC   * [List_Vector_Search_Endpoints]($endpoints/List_Vector_Search_Endpoints) - List and query Vector Search endpoints.
-# MAGIC   * [Get_Vector_Search_Endpoint]($endpoints/Get_Vector_Search_Endpoint) - Get Vector Search endpoint details including its indexes
-# MAGIC
-# MAGIC ##### MLflow MLmodel notebooks
-# MAGIC * [Download_MLmodel]($objects/Download_MLmodel) - Download and display a model version's MLmodel artifact.
-# MAGIC * [Download_MLmodels]($objects/Download_MLmodels) - Download a list of MLmodel-s.
-# MAGIC
 # MAGIC ##### SQL queries for MLflow `models` (registered models) and `versions` (model versions) tables
 # MAGIC * Queries for Databricks tables for `models` and `versions` as returned by the MLflow API.
 # MAGIC   * Example:`select user_id, count(*) as num_models from models group by user_id order by num_models desc`
@@ -33,7 +30,34 @@
 # MAGIC * [Model_Version_Queries]($sql_analytics/Model_Version_Queries)
 # MAGIC * [Create_MLflow_Database]($sql_analytics/Create_MLflow_Database) - Create the `models` and `versions` tables.
 # MAGIC
-# MAGIC ##### Feature  table notebooks
+# MAGIC ##### MLflow MLmodel notebooks
+# MAGIC * [Download_MLmodel]($objects/Download_MLmodel) - Download and display a model version's MLmodel artifact.
+# MAGIC * [Download_MLmodels]($objects/Download_MLmodels) - Download a list of MLmodel-s.
+# MAGIC
+# MAGIC #####  Markdown Model report
+# MAGIC * [Detailed_Model_Report]($reports/Detailed_Model_Report) - full report for an MLflow model and its related objects
+# MAGIC   * Related objects: Run, experiment, registered model, model version and MLflow model objects 
+# MAGIC   * Sample entry point: `models:/my_model/123`
+# MAGIC   * [Markdown and JSON samples](https://github.com/amesar/mlflow-reports/tree/master/samples/databricks/model_reports)
+
+# COMMAND ----------
+
+# MAGIC %md ### Endpoints
+# MAGIC
+# MAGIC ##### Model Serving endpoints aka MLflow deployment endpoints
+# MAGIC * [List_Model_Serving_Endpoints]($endpoints/List_Model_Serving_Endpoints) - List and query endpoints.
+# MAGIC   * [List_Model_Serving_Endpoint_Entities]($endpoints/List_Model_Serving_Endpoint_Entities) - List and query endpoint entities.
+# MAGIC   * [List_Model_Serving_Endpoint_Models]($endpoints/List_Model_Serving_Endpoint_Models) - List and query endpoint models/entities broken down by entity type.
+# MAGIC * [Get_Model_Serving_Endpoint]($endpoints/Get_Model_Serving_Endpoint) 
+# MAGIC
+# MAGIC ##### Vector Search endpoints
+# MAGIC * [List_Vector_Search_Endpoints]($endpoints/List_Vector_Search_Endpoints) - List and query Vector Search endpoints.
+# MAGIC * [List_Vector_Search_Indexes]($endpoints/List_Vector_Search_Indexes) - List and query all indexes.
+# MAGIC * [Get_Vector_Search_Endpoint]($endpoints/Get_Vector_Search_Endpoint) - Get Vector Search endpoint details including its indexes.
+
+# COMMAND ----------
+
+# MAGIC %md ### Feature tables
 # MAGIC
 # MAGIC * List feature tables
 # MAGIC   * [List_Feature_Tables]($feature_store/List_Feature_Tables) - List and query feature tables.
@@ -43,24 +67,18 @@
 # MAGIC   * [Get_Feature_Tables]($feature_store/Get_Feature_Tables) - Get JSON API response for feature tables.
 # MAGIC * [Get_Feature_Table]($feature_store/Get_Feature_Table) - Get feature table details.
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md ### Misc
+# MAGIC
 # MAGIC ##### Unity Catalog notebooks
 # MAGIC * [Get_Table]($unity_catalog/Get_Table) - get table details
 # MAGIC * [Get_Table_Lineage]($unity_catalog/Get_Table_Lineage) - get table lineage
 # MAGIC
-# MAGIC #####  Markdown Model report
-# MAGIC * [Detailed_Model_Report]($reports/Detailed_Model_Report) - full report for an MLflow model and its related objects
-# MAGIC   * Related objects: Run, experiment, registered model, model version and MLflow model objects 
-# MAGIC   * Sample entry point: `models:/my_model/123`
-# MAGIC   * [Markdown and JSON samples](https://github.com/amesar/mlflow-reports/tree/master/samples/databricks/model_reports)
-# MAGIC
 # MAGIC ##### Common helper utilities
 # MAGIC * [Common]($Common) - [_Common]($_Common)
 # MAGIC
-# MAGIC ##### Git Repo
-# MAGIC * https://github.com/amesar/mlflow-reports
-# MAGIC * https://github.com/amesar/mlflow-reports/tree/master/databricks_notebooks
-# MAGIC
-# MAGIC #####  Last updated: _2024-02-24_
 
 # COMMAND ----------
 
---
+++ databricks_notebooks/endpoints/List_Vector_Search_Indexes.py
@@ -0,0 +1,87 @@
+# Databricks notebook source
+# MAGIC %md ## List and Query Vector Search Indexes
+# MAGIC
+# MAGIC * Query all vector search indexes.
+
+# COMMAND ----------
+
+!pip install databricks-vectorsearch
+dbutils.library.restartPython() 
+
+# COMMAND ----------
+
+# MAGIC %run ../_Common
+
+# COMMAND ----------
+
+# MAGIC %md #### Create indexes dataframe
+
+# COMMAND ----------
+
+from databricks.vector_search.client import VectorSearchClient
+client = VectorSearchClient(disable_notice=True) 
+
+# COMMAND ----------
+
+endpoints = client.list_endpoints()["endpoints"]
+indexes = [ client.list_indexes(ep["name"])["vector_indexes"] for ep in endpoints ]
+indexes = [ _idx for _indexes in indexes for _idx in _indexes]
+len(indexes)
+
+# COMMAND ----------
+
+# MAGIC %md #### SQL queries
+
+# COMMAND ----------
+
+df = to_dataframe(indexes)
+df.createOrReplaceTempView("indexes") 
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show by endpoint name
+
+# COMMAND ----------
+
+# MAGIC %sql select endpoint_name, name, creator, primary_key, index_type from indexes order by endpoint_name, name
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show by index name
+
+# COMMAND ----------
+
+# MAGIC %sql select name, endpoint_name, creator, primary_key, index_type from indexes order by name, endpoint_name
+
+# COMMAND ----------
+
+# MAGIC %md ##### Show by creator
+
+# COMMAND ----------
+
+# MAGIC %sql select creator, endpoint_name, name, primary_key, index_type from indexes order by creator, endpoint_name, name
+
+# COMMAND ----------
+
+# MAGIC %md ##### Group by creator
+
+# COMMAND ----------
+
+# MAGIC %sql select creator, count(*) as num_indexes from indexes group by creator order by num_indexes desc
+
+# COMMAND ----------
+
+# MAGIC %md ##### Group by index_type
+
+# COMMAND ----------
+
+# MAGIC %sql select index_type, count(*) as count from indexes group by index_type order by count desc
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql select primary_key, count(*) as count from indexes group by primary_key order by count desc
