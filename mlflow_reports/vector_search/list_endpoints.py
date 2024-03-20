6c55c7b3b7e3a10726ddc7e2eaf3fe9730982405
amesar <amesar@users.noreply.github.co>
2024-03-19 20:58:19 -0400

    added vector_search/list_indexes.py

--- mlflow_reports/vector_search/list_endpoints.py
+++ mlflow_reports/vector_search/list_endpoints.py
@@ -4,11 +4,21 @@ from mlflow_reports.list.click_options import opt_columns
 from mlflow_reports.common import io_utils
 from . import get_VectorSearchClient
 
-client = get_VectorSearchClient()
+def list_endpoints():
+    client = get_VectorSearchClient()
+    return client.list_endpoints()["endpoints"]
 
+def as_pandas_df(endpoints):
+    from mlflow_reports.list import list_utils
+    from mlflow_reports.common.pandas_utils import move_column
+    import pandas as pd
+    df = pd.DataFrame.from_dict(endpoints)
+    df = move_column(df, "num_indexes", index=1)
+    list_utils.to_datetime(df, ["creation_timestamp", "last_updated_timestamp"])
+    return df
 
-def list_endpoints(columns, output_file_base):
-    endpoints = client.list_endpoints()["endpoints"]
+def show(columns, output_file_base):
+    endpoints = list_endpoints()
     print(f"Found {len(endpoints)}")
     ts_columns = [ "creation_timestamp", "last_updated_timestamp" ]
     io_utils.write_csv_and_json_files(output_file_base, endpoints, columns, ts_columns)
@@ -24,7 +34,7 @@ def main(columns, output_file_base):
         print(f"  {k}: {v}")
     if isinstance(columns, str):
         columns = columns.split(",")
-    list_endpoints(columns, output_file_base)
+    show(columns, output_file_base)
 
 if __name__ == "__main__":
     main()
---
+++ mlflow_reports/vector_search/list_indexes.py
@@ -0,0 +1,57 @@
+# https://mlflow.org/docs/latest/python_api/mlflow.gateway.html
+
+import pandas as pd
+import click
+from mlflow_reports.list import list_utils
+from mlflow_reports.common.click_options import opt_output_file_base
+from mlflow_reports.list.click_options import opt_columns
+from mlflow_reports.endpoints.click_options import opt_endpoint
+from mlflow_reports.common import io_utils
+from mlflow_reports.common.pandas_utils import move_column
+from . import get_VectorSearchClient
+
+client = get_VectorSearchClient()
+        
+def list_all_indexes():
+    endpoints = client.list_endpoints()["endpoints"]
+    indexes = [ client.list_indexes(ep["name"]).get("vector_indexes",[]) for ep in endpoints ]
+    indexes = [ _idx for _indexes in indexes for _idx in _indexes]
+    return endpoints
+
+def list_indexes(endpoint_name):
+    return client.list_indexes(endpoint_name)["vector_indexes"]
+
+
+def show(endpoint_name, columns, output_file_base):
+    if endpoint_name:
+        indexes = list_indexes(endpoint_name)
+    else:
+        indexes = list_all_indexes()
+    print(f"Found {len(indexes)} for endpoint {endpoint_name}")
+    io_utils.write_csv_and_json_files(output_file_base, indexes, columns)
+
+
+def as_pandas_df(indexes):
+    df = pd.DataFrame.from_dict(indexes)
+    del df["endpoint_name"]
+    move_column(df, "creator", index=1)
+    df = df.sort_values(["name"])
+    list_utils.to_datetime(df, ["creation_timestamp", "last_updated_timestamp"])
+    return df
+
+
+@click.command()
+@opt_endpoint
+@opt_columns
+@opt_output_file_base
+def main(endpoint, columns, output_file_base):
+    print("Options:")
+    for k,v in locals().items():
+        print(f"  {k}: {v}")
+    if isinstance(columns, str):
+        columns = columns.split(",")
+    show(endpoint, columns, output_file_base)
+
+
+if __name__ == "__main__":
+    main()
