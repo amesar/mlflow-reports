1ab743e816a68870cb05ba6e498a934cc9ed46a6
amesar <amesar@users.noreply.github.co>
2024-06-15 14:24:53 -0400

    dump_utils.py: added title argument to dump_object()

--- mlflow_reports/common/dump_utils.py
+++ mlflow_reports/common/dump_utils.py
@@ -5,7 +5,7 @@ def obj_to_dict(obj):
     """ Recursively convert an object to a dict. """
     return json.loads(
         json.dumps(obj, default=lambda o: getattr(o, '__dict__', str(o)))
-    )   
+    )
 
 
 def dict_to_json(dct, sort_keys=None, indent=2):
@@ -24,9 +24,8 @@ def dump_obj_as_json(obj, title=None):
     dump_as_json(obj_to_dict(obj))
 
 
-def dump_object(obj):
-    ##title = f"{type(obj).__name__}"
-    title = f"{type(obj)}"
+def dump_object(obj, title=None):
+    title = f"{title} - {type(obj)}" if title else f"{type(obj)}"
     print(f"{title}:")
     for k,v in obj.__dict__.items():
         print(f"  {k}: {v}")
